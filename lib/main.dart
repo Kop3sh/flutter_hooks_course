0e631652151ad9a3465afc65f77db0d865fe3868
Kop3sh <kirollosadel24@yahoo.com>
2022-07-10 17:38:55 +0200

    created basic future with caching and consuming the future later

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(
@@ -12,35 +22,26 @@ void main() {
   );
 }
 
+const url = 'https://bit.ly/3qYOtDm';
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
+    // caches complex objects
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
 
+    // consumes the future
+    final snapshot = useFuture(future);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed: ${text.value}'),
-        ],
-      ),
+      body: Column(children: [snapshot.data].compactMap().toList()),
     );
   }
 }
