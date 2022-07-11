80076586614c170e166860de881aa4c97c38f101
Kop3sh <kirollosadel24@yahoo.com>
2022-07-11 17:00:21 +0200

    created coutdown, utlizing memoization and notify listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 extension CompactMap<T> on Iterable<T?> {
@@ -24,24 +25,36 @@ void main() {
 
 const url = 'https://bit.ly/3qYOtDm';
 
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((value) {
+      this.value = value;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // caches complex objects
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then((data) => data.buffer.asUint8List())
-        .then((data) => Image.memory(data)));
-
-    // consumes the future
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
+    print('hello');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(children: [snapshot.data].compactMap().toList()),
+      body: Text(notifier.value.toString()),
     );
   }
 }
